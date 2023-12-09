6eb29328545a3c0d4c27c7d46938d9b2f0feda41
liqowomo <123171512+liqowomo@users.noreply.github.com>
2023-12-09 04:00:25 +0000

    .

--- W/sb/sb1.js
+++ W/sb/sb1.js
@@ -10,22 +10,22 @@ import {createClient} from '@supabase/supabase-js'
 // 1 .Creating the client
 const supabase = createClient(SBurl, SBkey)
 
+/**
+ * Write the function for fetching data , note 
+ * we are using the async function to prevent blocking of other 
+ * functions. Seems to be best practice 
+ */
 //2. Write function to fetch data
 
 async function getData() {
 	const {data, error} = await supabase.from('countries').select()
-
-	console.log(`
-
-======================
-Query results are here
-=======================
-
-`)
-	console.log(data, error)
+	if (error) {
+		console.log(`Data Fetching error`, error)
+	}
+	console.log(data)
 }
 
-// Calling all the functions
+// C
 
 // Fetch data
 getData()
