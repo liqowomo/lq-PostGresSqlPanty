3aed4148dd963fa849625ecbeee0744da3359e43
liqowomo <123171512+liqowomo@users.noreply.github.com>
2023-12-13 16:27:03 +0000

    .

---
+++ W/sb/mytests1/sb1v2.js
@@ -0,0 +1,40 @@
+/**
+ * This for testing the various commands from
+ * https://supabase.com/docs/reference/javascript/installing
+ */
+
+// Importing relevant shit
+import {createClient} from '@supabase/supabase-js'
+import {SBurl} from './config'
+import {SBkey} from './config'
+
+// Initiate supabase client
+const supabase = createClient(SBurl, SBkey)
+
+// Running ascynchronous fetch commands
+
+export async function getData() {
+	try {
+		const {data, error} = await supabase.from('countries').select()
+		console.log(data)
+	} catch (error) {
+		console.log(error)
+	}
+}
+
+// Get version of the databse
+async function getPostgresVersion() {
+	const {data, error} = await supabase.rpc('get_postgres_version')
+
+	if (error) {
+		console.error('Error fetching PostgreSQL version:', error)
+		return
+	}
+
+	const postgresVersion = data[0].get_postgres_version
+	console.log('PostgreSQL version:', postgresVersion)
+}
+
+// Execute command
+getData()
+// getPostgresVersion()
