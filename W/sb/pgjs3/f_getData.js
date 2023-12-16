1c9ae5fa1cdc24cd27b8f0b32a83dc3922c8d6ce
liqowomo <123171512+liqowomo@users.noreply.github.com>
2023-12-16 10:07:45 +0000

    .

---
+++ W/sb/pgjs3/f_getData.js
@@ -0,0 +1,28 @@
+/* 
+This is for getting data
+*/
+
+import sql from './db.js'
+import fs from 'fs'
+
+// makign thet Table
+async function getData() {
+	try {
+		const data = await sql`
+		select * from countries
+		
+    `
+
+		// Code for writing to a gile
+		// Write the data to a file
+		fs.writeFileSync('outs/getData.json', JSON.stringify(data))
+
+		console.log(data)
+	} catch (error) {
+		console.error('An error occurred:', error)
+		fs.writeFileSync('outs/getDataError.txt', JSON.stringify(error))
+	}
+	process.exit(0)
+}
+// Execute the code
+getData()
