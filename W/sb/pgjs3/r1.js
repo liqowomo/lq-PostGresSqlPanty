cc1707626bd414a825afb7b86db3c91f9ae94989
liqowomo <123171512+liqowomo@users.noreply.github.com>
2023-12-16 08:53:29 +0000

    .

--- W/sb/pgjs3/r1.js
+++ W/sb/pgjs3/r1.js
@@ -1,4 +1,5 @@
 import sql from './db.js'
+import fs from 'fs'
 
 async function getData() {
 	try {
@@ -7,28 +8,18 @@ async function getData() {
       *
       from countries
     `
+
+		// Code for writing to a gile
+		// Write the data to a file
+		fs.writeFileSync('getData.txt', JSON.stringify(data))
+
 		console.log(data)
 	} catch (error) {
 		console.error('An error occurred:', error)
+		fs.writeFileSync('getDataError.txt', JSON.stringify(error))
 	}
 	process.exit(0)
 }
 
 // Execute the code
 getData()
-
-//Drop a table
-async function dropTable() {
-	try {
-		const data = await sql`
-      drop table 
-	  countries
-    `
-		console.log(data)
-	} catch (error) {
-		console.error('An error occurred:', error)
-	}
-	process.exit(0)
-}
-//Execute code
-// dropTable()
