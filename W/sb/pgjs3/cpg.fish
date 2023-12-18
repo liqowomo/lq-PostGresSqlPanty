e1926b3b3e5c798066ea719db2410b017e606ffc
liqowomo <123171512+liqowomo@users.noreply.github.com>
2023-12-18 12:04:15 +0000

    .

--- W/sb/pgjs3/cpg.fish
+++ W/sb/pgjs3/cpg.fish
@@ -1,8 +1,16 @@
 #!/usr/bin/env fish 
+clear
+echo ""
+echo " Connceting using pgcli"
+echo " Docs - https://www.pgcli.com/docs"
 echo ""
 echo "Connnecting to the postgres db using pgcli"
+echo "- This is alternative method of connecting to the db insread of psql , pgcli has more colors and stuff"
 echo ""
 echo "Main Command from website"
+echo ""
 echo "pgcli -h db.urlqwzlpqnxiduxiwdse.supabase.co -p 5432 -d postgres -U postgres"
-echo "Password: YermaGEx4wthRB3W"
+echo ""
+echo (set_color red)"Password: YermaGEx4wthRB3W" 
+echo ""
 pgcli -h db.urlqwzlpqnxiduxiwdse.supabase.co -p 5432 -d postgres -U postgres
