934cce62f686c17f3c6967df04a9e599070c8b26
liqowomo <123171512+liqowomo@users.noreply.github.com>
2023-12-18 14:25:31 +0000

    .

---
+++ W/sb/pgjs3/S_calcData.js
@@ -0,0 +1,37 @@
+/* 
+Insering Data into table cityfuck
+
+*/
+
+import sql from './db.js'
+import fs from 'fs'
+
+// Constants
+
+const good = 'outs/ScalcData.json'
+const bad = 'outs/ScalcDataError.json'
+
+// makign thet Table
+async function ScalcData() {
+	try {
+		const data = await sql`
+		-- Running Transforms which are calculations
+		-- Make a new calculated column 
+		select 
+		name, population/area, name 
+		from cityfuck
+		
+    `
+		// Code for writing to a gile
+		// Write the data to a file
+		fs.writeFileSync(good, JSON.stringify(data))
+
+		console.log(data)
+	} catch (error) {
+		console.error('FuckYouBastard:', error)
+		fs.writeFileSync('outs/getDataError.txt', JSON.stringify(error))
+	}
+	process.exit(0)
+}
+// Execute the code
+ScalcData()
