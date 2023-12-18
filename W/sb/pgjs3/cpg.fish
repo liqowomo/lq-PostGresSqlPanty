812e68dc507b1c5ca13ffaa6625e3b5481879633
liqowomo <123171512+liqowomo@users.noreply.github.com>
2023-12-18 12:00:26 +0000

    .

---
+++ W/sb/pgjs3/cpg.fish
@@ -0,0 +1,8 @@
+#!/usr/bin/env fish 
+echo ""
+echo "Connnecting to the postgres db using pgcli"
+echo ""
+echo "Main Command from website"
+echo "pgcli -h db.urlqwzlpqnxiduxiwdse.supabase.co -p 5432 -d postgres -U postgres"
+echo "Password: YermaGEx4wthRB3W"
+pgcli -h db.urlqwzlpqnxiduxiwdse.supabase.co -p 5432 -d postgres -U postgres
